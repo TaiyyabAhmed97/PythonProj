fc5832bd8388a4e5d9332439d081b2e42013723f
Taiyyab Ahmed <hafiztaiyyab11@gmail.com>
2018-02-24 02:19:21 -0600

    added db

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 from flask import Flask
-from flask_pymongo import PyMongo
+from flask import jsonify
+from flask import request
+from mongoengine import *
+connect('mongoengine_test', host='localhost', port=27017)
 
 app = Flask(__name__)
-mongo = PyMongo(app)
+
+class User(Document):
+    name = StringField(required=True, max_length=200)
 
 @app.route("/")
 def hello():
+    post_1 = User(
+    name='Ali'
+    )
+    post_1.save()
+    print(post_1.name)
     return "Hello World!"
 #first commen
