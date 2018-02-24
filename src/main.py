370728510ecedb87b117c1ffb265a4bb822f3e37
Taiyyab Ahmed <hafiztaiyyab11@gmail.com>
2018-02-24 03:46:35 -0600

    added username

--- src/main.py
+++ src/main.py
@@ -7,14 +7,9 @@ connect('mongoengine_test', host='localhost', port=27017)
 app = Flask(__name__)
 
 class User(Document):
-    name = StringField(required=True, max_length=200)
+    username = StringField(required=True, max_length=200)
 
 @app.route("/")
 def hello():
-    post_1 = User(
-    name='Ali'
-    )
-    post_1.save()
-    print(post_1.name)
     return "Hello World!"
 #first commen
