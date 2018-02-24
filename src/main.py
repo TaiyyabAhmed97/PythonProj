db42ee9256b21c461d0a2de593525dfd37975835
Zakariah Siyaji <z.siyaji@gmail.com>
2018-02-24 03:45:33 -0600

    Added login post method

--- src/main.py
+++ src/main.py
@@ -7,14 +7,12 @@ connect('mongoengine_test', host='localhost', port=27017)
 app = Flask(__name__)
 
 class User(Document):
-    name = StringField(required=True, max_length=200)
+    username = StringField(required=True, max_length=200)
+    password = StringField(required=True, max_length=200)
 
-@app.route("/")
-def hello():
-    post_1 = User(
-    name='Ali'
-    )
-    post_1.save()
-    print(post_1.name)
-    return "Hello World!"
-#first commen
+@app.route('/login', methods=['POST'])
+def login():
+    person = User()
+    person.save()
+    print(person.name)
+    return render_template('login.html', error=error)
